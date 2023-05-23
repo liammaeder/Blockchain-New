d59181c91f4e2d3c0a0f854983c3bd39118e7c7f
Liam Maeder <55049055+liammaeder@users.noreply.github.com>
2023-05-24 00:45:45 +0200

    Update blockchain.js

--- src/blockchain.js
+++ src/blockchain.js
@@ -2,182 +2,325 @@ const crypto = require('crypto');
 const EC     = require('elliptic').ec;
 const ec     = new EC('secp256k1');
 
-class Transaction{
-    constructor(fromAddress, toAddress, amount){
+class Transaction {
+    /**
+     * @param {string} fromAddress
+     * @param {string} toAddress
+     * @param {number} amount
+     */
+    constructor(fromAddress, toAddress, amount) {
         this.fromAddress = fromAddress;
-        this.toAddress   = toAddress;
-        this.amount      = amount;
-        this.timestamp   = Date.now();
+        this.toAddress = toAddress;
+        this.amount = amount;
+        this.timestamp = Date.now();
     }
 
-    calculateHash(){
-        return crypto.createHash('sha256').update(this.fromAddress + this.toAddress + this.amount + this.timestamp).digest('hex');
+    /**
+     * Creates a SHA256 hash of the transaction
+     *
+     * @returns {string}
+     */
+    calculateHash() {
+        return crypto
+            .createHash('sha256')
+            .update(this.fromAddress + this.toAddress + this.amount + this.timestamp)
+            .digest('hex');
     }
 
-    signTransation(signingKey){
-        if(signingKey.getPublic('hex') !== this.fromAddress){
-            throw new Error('You cannot sign transactions for other wallets.');
+    /**
+     * Signs a transaction with the given signingKey (which is an Elliptic keypair
+     * object that contains a private key). The signature is then stored inside the
+     * transaction object and later stored on the blockchain.
+     *
+     * @param {string} signingKey
+     */
+    sign(signingKey) {
+        // You can only send a transaction from the wallet that is linked to your
+        // key. So here we check if the fromAddress matches your publicKey
+        if (signingKey.getPublic('hex') !== this.fromAddress) {
+            throw new Error('You cannot sign transactions for other wallets!');
         }
 
-        const hashTx   = this.calculateHash();
-        const sig      = signingKey.sign(hashTx, 'base64');
+        // Calculate the hash of this transaction, sign it with the key
+        // and store it inside the transaction object
+        const hashTx = this.calculateHash();
+        const sig = signingKey.sign(hashTx, 'base64');
+
         this.signature = sig.toDER('hex');
     }
 
-    isValid(){
-        //mining reward
-        if(this.fromAddress === null) return true;
+    /**
+     * Checks if the signature is valid (transaction has not been tampered with).
+     * It uses the fromAddress as the public key.
+     *
+     * @returns {boolean}
+     */
+    isValid() {
+        // If the transaction doesn't have a from address we assume it's a
+        // mining reward and that it's valid. You could verify this in a
+        // different way (special field for instance)
+        if (this.fromAddress === null) return true;
 
-        //empty signature
-        if(!this.signature || this.signature.length === 0){
-            throw new Error('No signature in this transaction.');
+        if (!this.signature || this.signature.length === 0) {
+            throw new Error('No signature in this transaction');
         }
 
-        //check that signature matches
         const publicKey = ec.keyFromPublic(this.fromAddress, 'hex');
         return publicKey.verify(this.calculateHash(), this.signature);
     }
 }
 
-class Block{
-    constructor(timestamp, transactions, previousHash = ''){
+class Block {
+    /**
+     * @param {number} timestamp
+     * @param {Transaction[]} transactions
+     * @param {string} previousHash
+     */
+    constructor(timestamp, transactions, previousHash = '') {
         this.previousHash = previousHash;
-        this.timestamp    = timestamp;
+        this.timestamp = timestamp;
         this.transactions = transactions;
-        this.nonce        = 0;
-        this.hash         = this.calculateHash();
+        this.nonce = 0;
+        this.hash = this.calculateHash();
     }
 
-    calculateHash(){
-        const val = crypto.createHash('sha256').update(this.previousHash + this.timestamp + JSON.stringify(this.transactions) + this.nonce).digest('hex');
-        return val;
+    /**
+     * Returns the SHA256 of this block (by processing all the data stored
+     * inside this block)
+     *
+     * @returns {string}
+     */
+    calculateHash() {
+        return crypto
+            .createHash('sha256')
+            .update(
+                this.previousHash +
+                this.timestamp +
+                JSON.stringify(this.transactions) +
+                this.nonce
+            )
+            .digest('hex');
     }
 
-    mineBlock(difficulty){
-        while(this.hash.substring(0, difficulty) !== Array(difficulty + 1).join('0')){
+    /**
+     * Starts the mining process on the block. It changes the 'nonce' until the hash
+     * of the block starts with enough zeros (= difficulty)
+     *
+     * @param {number} difficulty
+     */
+    mineBlock(difficulty) {
+        while (
+            this.hash.substring(0, difficulty) !== Array(difficulty + 1).join('0')
+            ) {
             this.nonce++;
             this.hash = this.calculateHash();
         }
-        console.log("Block mined: " + this.hash);
     }
 
-    hasValidTransactions(){
-        for(const tx of this.transactions){
-            if(!tx.isValid()){
+    /**
+     * Validates all the transactions inside this block (signature + hash) and
+     * returns true if everything checks out. False if the block is invalid.
+     *
+     * @returns {boolean}
+     */
+    hasValidTransactions() {
+        for (const tx of this.transactions) {
+            if (!tx.isValid()) {
                 return false;
             }
         }
+
         return true;
     }
 }
 
-class Blockchain{
-    constructor(){
-        this.chain               = [this.createGenesisBlock()];
-        this.difficulty          = 2;
+class Blockchain {
+    constructor() {
+        this.chain = [this.createGenesisBlock()];
+        this.difficulty = 2;
         this.pendingTransactions = [];
-        this.miningReward        = 100;
+        this.miningReward = 100;
     }
 
-    createGenesisBlock(){  
-        return new Block(Date.parse('2023-05-20'), [], '0');
+    /**
+     * @returns {Block}
+     */
+    createGenesisBlock() {
+        return new Block(Date.parse('2017-01-01'), [], '0');
     }
 
-    getLatestBlock(){
+    /**
+     * Returns the latest block on our chain. Useful when you want to create a
+     * new Block and you need the hash of the previous Block.
+     *
+     * @returns {Block[]}
+     */
+    getLatestBlock() {
         return this.chain[this.chain.length - 1];
     }
 
-    minePendingTransactions(miningRewardAddress){
-        let block = new Block(Date.now(), this.pendingTransactions, this.getLatestBlock().hash);
+    /**
+     * Takes all the pending transactions, puts them in a Block and starts the
+     * mining process. It also adds a transaction to send the mining reward to
+     * the given address.
+     *
+     * @param {string} miningRewardAddress
+     */
+    minePendingTransactions(miningRewardAddress) {
+        const rewardTx = new Transaction(
+            null,
+            miningRewardAddress,
+            this.miningReward
+        );
+        this.pendingTransactions.push(rewardTx);
+
+        const block = new Block(
+            Date.now(),
+            this.pendingTransactions,
+            this.getLatestBlock().hash
+        );
         block.mineBlock(this.difficulty);
 
-        console.log('Block successfully mined.');
         this.chain.push(block);
 
-        this.pendingTransactions = [
-            new Transaction(null, miningRewardAddress, this.miningReward)
-        ];
+        this.pendingTransactions = [];
     }
 
-    addTransaction(transaction){
-        //ensure from and to addresses are filled in
-        if(!transaction.fromAddress || !transaction.toAddress){
-            throw new Error('Transaction must include from and to address.');
+    /**
+     * Add a new transaction to the list of pending transactions (to be added
+     * next time the mining process starts). This verifies that the given
+     * transaction is properly signed.
+     *
+     * @param {Transaction} transaction
+     */
+    addTransaction(transaction) {
+        if (!transaction.fromAddress || !transaction.toAddress) {
+            throw new Error('Transaction must include from and to address');
         }
 
-        //verify that transaction is valid
-        if(!transaction.isValid()){
-            throw new Error('Cannot add invalid transaction to chain.');
+        // Verify the transactiion
+        if (!transaction.isValid()) {
+            throw new Error('Cannot add invalid transaction to chain');
         }
 
-        //Ensure amount is higher than 0
-        if(transaction.amount <= 0){
-            throw new Error('Transaction amount should be higher than 0')
+        if (transaction.amount <= 0) {
+            throw new Error('Transaction amount should be higher than 0');
         }
-        /*
-        //Ensure amount sent is not more than existing balance
+
+        // Making sure that the amount sent is not greater than existing balance
         const walletBalance = this.getBalanceOfAddress(transaction.fromAddress);
-        if(walletBalance < transaction.amount){
-            throw new Error('Balance too low to perform transaction.');
+        if (walletBalance < transaction.amount) {
+            throw new Error('Not enough balance');
         }
 
-        //Get pending transactions from wallet
-        const pendingTxForWallet = this.pendingTransactions.filter(tx => tx.fromAddress === transaction.fromAddress);
+        // Get all other pending transactions for the "from" wallet
+        const pendingTxForWallet = this.pendingTransactions.filter(
+            tx => tx.fromAddress === transaction.fromAddress
+        );
 
-        //If wallet has pending transactions calculate total amount of spend coins and refuse transaction if balance is exceeded
-        if(pendingTxForWallet.length > 0){
-            const totalPendingAmount = pendingTxForWallet.map(tx => tx.amount).reduce((prev,curr) => prev + curr);
-            const totalAmount        = totalPendingAmount + transaction.amount;
+        // If the wallet has more pending transactions, calculate the total amount
+        // of spend coins so far. If this exceeds the balance, we refuse to add this
+        // transaction.
+        if (pendingTxForWallet.length > 0) {
+            const totalPendingAmount = pendingTxForWallet
+                .map(tx => tx.amount)
+                .reduce((prev, curr) => prev + curr);
 
-            if(totalAmount > walletBalance){
-                throw new Error('Pending transactions for this wallet is higher than its balance.');
+            const totalAmount = totalPendingAmount + transaction.amount;
+            if (totalAmount > walletBalance) {
+                throw new Error(
+                    'Pending transactions for this wallet is higher than its balance.'
+                );
             }
-        }*/
+        }
+
         this.pendingTransactions.push(transaction);
-        console.log('Transaction added: ' + transaction);
     }
-    
-    getBalanceOfAddress(address){
+
+    /**
+     * Returns the balance of a given wallet address.
+     *
+     * @param {string} address
+     * @returns {number} The balance of the wallet
+     */
+    getBalanceOfAddress(address) {
         let balance = 0;
 
-        for(const block of this.chain){
-            for(const trans of block.transactions){
-                if(trans.fromAddress === address){
+        for (const block of this.chain) {
+            for (const trans of block.transactions) {
+                if (trans.fromAddress === address) {
                     balance -= trans.amount;
                 }
 
-                if(trans.toAddress === address){
+                if (trans.toAddress === address) {
                     balance += trans.amount;
                 }
             }
         }
+
         return balance;
     }
 
-    isChainValid(){
-        for(let i = 1; i < this.chain.length; i++){
-            const currentBlock  = this.chain[i];
+    /**
+     * Returns a list of all transactions that happened
+     * to and from the given wallet address.
+     *
+     * @param  {string} address
+     * @return {Transaction[]}
+     */
+    getAllTransactionsForWallet(address) {
+        const txs = [];
+
+        for (const block of this.chain) {
+            for (const tx of block.transactions) {
+                if (tx.fromAddress === address || tx.toAddress === address) {
+                    txs.push(tx);
+                }
+            }
+        }
+
+        return txs;
+    }
+
+    /**
+     * Loops over all the blocks in the chain and verify if they are properly
+     * linked together and nobody has tampered with the hashes. By checking
+     * the blocks it also verifies the (signed) transactions inside of them.
+     *
+     * @returns {boolean}
+     */
+    isChainValid() {
+        // Check if the Genesis block hasn't been tampered with by comparing
+        // the output of createGenesisBlock with the first block on our chain
+        const realGenesis = JSON.stringify(this.createGenesisBlock());
+
+        if (realGenesis !== JSON.stringify(this.chain[0])) {
+            return false;
+        }
+
+        // Check the remaining blocks on the chain to see if there hashes and
+        // signatures are correct
+        for (let i = 1; i < this.chain.length; i++) {
+            const currentBlock = this.chain[i];
             const previousBlock = this.chain[i - 1];
 
-            //if any transactions are invalid
-            if(!currentBlock.hasValidTransactions()){
+            if (previousBlock.hash !== currentBlock.previousHash) {
                 return false;
             }
 
-            // check if current block's data has been changed
-            if(currentBlock.hash !== currentBlock.calculateHash()){
+            if (!currentBlock.hasValidTransactions()) {
                 return false;
             }
-            
-            // Does current block point to previous block
-            if(currentBlock.previousHash !== previousBlock.hash){
+
+            if (currentBlock.hash !== currentBlock.calculateHash()) {
                 return false;
             }
         }
+
         return true;
     }
-    // add functionality to roll back to previous correct block in case of tampering
 }
 
+module.exports.Blockchain = Blockchain;
+module.exports.Block = Block;
 module.exports.Transaction = Transaction;
-module.exports.Blockchain  = Blockchain;
